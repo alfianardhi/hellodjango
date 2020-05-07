18f75234b0f6687067e1d6b6820e7bb2581c9e42
alfianardhi <ardhi.alfian@gmail.com>
2020-05-08 01:15:32 +0700

    [app01] update simple access menus

--- mydjango/myapp/views.py
+++ mydjango/myapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('<h1>Ini Home Page</h1>')
+
+def about(request):
+    return  HttpResponse('<h1>Ini About Page</h1>')
--- mydjango/mydjango/urls.py
+++ mydjango/mydjango/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('about/', views.about),
 ]
